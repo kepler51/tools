909ec377074b81909d954d96a1e4f5a4eeb90dfa
Jared Lovell <jlovell@kepler51.com>
2026-01-15 11:35:42 -0800

    mp3s

--- multicast_poc/serv_g722.sh
+++ multicast_poc/serv_g722.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ADDR=224.0.0.1:4567
+SOUNDFILE=test.mp3
+SOUNDFILE=bb_03.mp3
+SOUNDFILE=440Hz_44100Hz_16bit_30sec.mp3
+
 
 #examples that work
 #ffmpeg -re -i test.mp3 -c:v copy -c:a copy -f rtp rtp://239.1.2.3:4567?ttl=8
@@ -9,4 +13,4 @@ ADDR=224.0.0.1:4567
 #doesn't quite work
 #ffmpeg -re -i test.mp3 -c:a g722 -f rtp rtp://${ADDR}?ttl=8
 
-ffmpeg -re -i test.mp3 -c:a g722 -ar 16000 -sdp_file saved_sdp_file -f rtp rtp://${ADDR}?ttl=8
+ffmpeg -re -i $SOUNDFILE -c:a g722 -ar 16000 -sdp_file saved_sdp_file -f rtp rtp://${ADDR}?ttl=8
